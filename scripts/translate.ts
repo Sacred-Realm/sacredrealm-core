7fd335c85633daeb8b0c00886b8b037cf84a2f9d
havenquan <havenquan@qq.com>
2022-03-29 16:11:13 +0800

    trans

--- scripts/translate.ts
+++ scripts/translate.ts
@@ -1,16 +1,26 @@
 import fs from 'fs';
 
 async function translate() {
-    const files = fs.readdirSync(`text`);
+    const files = fs.readdirSync(`output`);
+    // for (let file of files) {
+    //     const json = JSON.parse(fs.readFileSync(`text/${file}`).toString());
+    //     const text = json.reduce((pre: any, cur: any) => pre + cur.Text_Korean + '\n', 0);
+    //     // fs.writeFileSync(`temp/${file}`, text);
+    //     const temp = fs.readFileSync(`temp/${file}`).toString().split('\n');
+    //     for (let i = 0; i < json.length; i++) {
+    //         json[i].Text_Korean = temp[i];
+    //     }
+    //     fs.writeFileSync(`output/${file}`, JSON.stringify(json));
+    // }
     for (let file of files) {
-        const json = JSON.parse(fs.readFileSync(`text/${file}`).toString());
-        const text = json.reduce((pre: any, cur: any) => pre + cur.Text_Korean + '\n', 0);
-        // fs.writeFileSync(`temp/${file}`, text);
-        const temp = fs.readFileSync(`temp/${file}`).toString().split('\n');
+        const json = JSON.parse(fs.readFileSync(`output/${file}`).toString());
+        const text = json.reduce((pre: any, cur: any) => pre + (cur.Text_English ? '' : cur.Text_Korean) + '\n', 0);
+        // fs.writeFileSync(`temp2/${file}`, text);
+        const temp = fs.readFileSync(`temp2/${file}`).toString().split('\n');
         for (let i = 0; i < json.length; i++) {
-            json[i].Text_Korean = temp[i];
+            if (temp[i]) json[i].Text_English = temp[i];
         }
-        fs.writeFileSync(`output/${file}`, JSON.stringify(json));
+        fs.writeFileSync(`output2/${file}`, JSON.stringify(json));
     }
 }
 
